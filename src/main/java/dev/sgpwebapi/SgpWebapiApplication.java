c6fa985e176d71796cb78bf63b4d3acf1497a36e
Clément BARBASTE <barbaste.clement@gmail.com>
2017-11-09 10:27:46 +0100

    Commit initial.

---
+++ src/main/java/dev/sgpwebapi/SgpWebapiApplication.java
@@ -0,0 +1,12 @@
+package dev.sgpwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgpWebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgpWebapiApplication.class, args);
+	}
+}
